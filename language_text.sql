f347790ce126c7ae12180321bfe2a470407065c0
Greg Gay <gregrgay@gmail.com>
2017-12-10 16:08:32 -0500

    Azerbaijani language pack commit

---
+++ language_text.sql
@@ -0,0 +1,12 @@
+INSERT INTO `languages` VALUES ('az', 'utf-8', 'ltr', 'az|azerbaijani', 'Azerbaijani', 'Azerbaijani', 1);
+
+INSERT INTO `language_text` VALUES ('az', '_msgs', 'AT_CONFIRM_ADD_FEED', 'Bu sindikatlaşdırılmış məlumatı əlavə etməyə əminsinizmi?', '2013-09-06 11:09:53', ''),
+('az', '_msgs', 'AT_CONFIRM_ADD_MODULE', 'Siz <strong> <code>%s </code> </strong> modul yükləməyə əminsinizmi?', '2013-09-06 11:10:57', ''),
+('az', '_msgs', 'AT_CONFIRM_ADD_TEST_QUESTIONS', 'Siz aşağıdakı sualları <em> əlavə etməyə </em> əminsinizmi? <ul>%s</ul>', '2013-09-06 11:12:40', ''),
+('az', '_msgs', 'AT_CONFIRM_ALUMNI', 'Siz  aşağıdakı istifadəçiləri kurs məzunları olaraq qeyd etməyə əminsinizmi: <ul> %s </ul>', '2013-09-06 11:13:55', ''),
+('az', '_msgs', 'AT_CONFIRM_DELETE', 'Bu maddələri silmək üçün əminsinizmi? <br/> <ul>%s </ul>', '2013-09-06 11:21:58', ''),
+('az', '_template', '15_max_chars', 'minimum 8 simvol, maksimum 15 simvol', '2012-02-21 15:21:52', ''),
+('az', '_template', '20_max_chars', 'maksimum 20 simvol', '2012-02-21 15:22:46', ''),
+('az', '_template', 'about_atutor', 'ATutor haqqında', '2012-03-03 11:29:58', ''),
+('az', '_template', 'welcome_to_atutor', 'Atutor-a Xoş Gəldiniz!', '2012-02-21 15:27:29', ''),
+('az', '_template', 'year', 'İl', '2012-02-21 15:26:37', '');
